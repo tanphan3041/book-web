c1cf837f0125b85d5efd26d8c66eaf17cd693c93
Minh Tân Phan <106483560+tanphan3041@users.noreply.github.com>
2022-11-27 15:18:43 +0700

    Project Progresses Continue 1

---
+++ frontend/src/views/BookAdd.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="page">
+      <h4>Thêm Sách</h4>
+      <BookForm :book="onAddBook" @submit:book="onAddBook" />
+      <p>{{ message }}</p>
+   </div>
+</template>
+
+<script>
+import BookForm from '@/components/BookForm.vue';
+import { bookService } from '@/services/book.service';
+export default {
+   components: {
+      BookForm,
+   },
+   data() {
+      return {
+         book: null,
+         message: '',
+      };
+   },
+   methods: {
+      async onAddBook(book) {
+         try {
+            await bookService.create(book)
+            this.message = 'Sách được thêm thành công.';
+         } catch (error) {
+            console.log(error);
+         }
+      },
+   },
+   //   created() {
+   //      this.onAddBook(this.book);
+   //      this.message = '';
+   //   },
+};
+</script>
